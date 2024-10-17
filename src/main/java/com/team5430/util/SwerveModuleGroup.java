aec9a5e1fd340d7e85e1dad9716e9f97e187a345
frc5430c <frc5430c@gmail.com>
2024-10-17 13:01:32 -0700

    Toned down config -RL

--- src/main/java/com/team5430/util/SwerveModuleGroup.java
+++ src/main/java/com/team5430/util/SwerveModuleGroup.java
@@ -3,8 +3,11 @@ package com.team5430.util;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.I2C.Port;
 
 import java.util.function.DoubleSupplier;
+
+import com.kauailabs.navx.frc.AHRS;
 
 
 
@@ -13,9 +16,10 @@ public class SwerveModuleGroup implements Sendable {
 //max is 4 swerve modules; accounted for array
   private SwerveModule[] swerveModules =  new SwerveModule[4];
   private int moduleCount;
+  private AHRS _gyro;
 
 
-  private DoubleSupplier gyroscope;
+  private double headingAngle;
   private double currentAngle;
   /* 
   private double forward;
@@ -57,6 +61,7 @@ moduleCount = ModuleCount;
       swerveModules[i] = new SwerveModule(i * 2, i * 2 + 1, i, config.STEERING_MODULE_OFFSET[i]);
     }
   
+    _gyro = new AHRS(Port.kMXP);
  
     //Translation2d -> location of the graph in swerve module is  Front is postive, Left is positive 
 //Measurement: Meters 
@@ -95,8 +100,8 @@ moduleCount = ModuleCount;
  }
   }
 
-  public void setGyro(DoubleSupplier GyroscopeAngle) {
-    gyroscope = GyroscopeAngle;
+  public void setGyro() {
+    headingAngle =_gyro.getAngle();
   }
 
   
@@ -105,29 +110,32 @@ moduleCount = ModuleCount;
     return 1 - input;
   }
 
-  public void getGyroAngle() {
-    currentAngle = gyroscope.getAsDouble();
+  public double getGyroAngle() {
+    return headingAngle;
   }
 
   public void Drive(double wantedAngle, double throttle, double thirdAxis, double Robotangle, double breaking) {
-
+    
     // if turning within range of deadzone;
-    if (thirdAxis > .3 || thirdAxis < -.3) {
+    if ((thirdAxis > .3 || thirdAxis < -.3) ) {
       // used to drive while turning
       double power = MathUtil.applyDeadband(thirdAxis, .3) + throttle;
 
       //adjust as needed for turning speed
-            setThrottle(power/5  * VariableSpeedDecline(breaking));
+            setThrottle(power/10  * VariableSpeedDecline(breaking));
 
-      setAngle(Robotangle - currentAngle);
+      setAngle(Robotangle - headingAngle); 
     } else {
       // when not turning
       setAngle(wantedAngle);
       
-      swerveModules[0].setThrottle(throttle/5 * VariableSpeedDecline(breaking));
-      swerveModules[1].setThrottle(-throttle/5 * VariableSpeedDecline(breaking));
-      swerveModules[2].setThrottle(-throttle/5 * VariableSpeedDecline(breaking));
-      swerveModules[3].setThrottle(throttle/5 * VariableSpeedDecline(breaking));
+      swerveModules[0].setThrottle(throttle/10 * VariableSpeedDecline(breaking));
+      swerveModules[1].setThrottle(-throttle/10 * VariableSpeedDecline(breaking));
+      swerveModules[2].setThrottle(-throttle/10 * VariableSpeedDecline(breaking));
+      swerveModules[3].setThrottle(throttle/10 * VariableSpeedDecline(breaking));
+    //save angle
+      setGyro();
+      
     }
 
   
